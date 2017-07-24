0fdec8a16cc9403059a65f780643d67a1ef74549
GitForShadow <773280154@qq.com>
2017-07-24 22:54:18 +0800

    front end push

---
+++ frontend/src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue';
+import iView from 'iview';
+import Axios from 'axios'
+import VueRouter from 'vue-router';
+import Routers from './router';
+import Util from './libs/util';
+import App from './app.vue';
+import 'iview/dist/styles/iview.css';
+
+Vue.use(VueRouter);
+Vue.use(iView);
+Vue.prototype.$http = Axios;
+
+Vue.prototype.url = "http://127.0.0.1:8000/";
+
+// 路由配置
+const RouterConfig = {
+    mode: 'history',
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    iView.LoadingBar.start();
+    Util.title(to.meta.title);
+    next();
+});
+
+router.afterEach((to, from, next) => {
+    iView.LoadingBar.finish();
+    window.scrollTo(0, 0);
+});
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
---
+++ frontend/src/router/index.js
@@ -0,0 +1,13 @@
+
+//首页
+import Home from '../views/home'
+const routers = [
+    {
+        path: '/',
+        meta: {
+            title: ''
+        },
+        component: Home
+    }
+];
+export default routers;
