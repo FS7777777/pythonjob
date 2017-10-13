d6a24388eb155f99e87bc3304054cebfead4d3ae
GitForShadow <773280154@qq.com>
2017-10-13 18:27:28 +0800

    bug releas

---
+++ frontend/src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import VueRouer from 'vue-router'
+//router path
+import column from 'components/column/column'
+import funnel from 'components/funnel/funnel'
+import heat from 'components/heat/heat'
+import point from 'components/point/point'
+import line from 'components/line/line'
+import dashboard from 'components/dashboard/dashboard'
+import multipleColumn from 'components/multipleColumn/multipleColumn'
+
+import home from 'src/views/home/home'
+
+Vue.use(VueRouer)
+export default new VueRouer({
+    routes: [{
+      path: '/column',
+      component: column
+    }, {
+      path: '/funnel',
+      component: funnel
+    }, {
+      path: '/heat',
+      component: heat
+    }, {
+      path: '/point',
+      component: point
+    }, {
+      path: '/dashboard',
+      component: dashboard
+    }, {
+      path: '/multipleColumn',
+      component: multipleColumn
+    }, {
+      path: '/line',
+      component: line
+    },{
+        path: '/home',
+        component: home
+    }
+],
+    linkActiveClass: 'active'
+})
---
+++ frontend/src/views/home/home.vue
@@ -0,0 +1,46 @@
+<<template>
+<div>
+    <Row :gutter="16">
+        <Col span="18">
+            <Carousel autoplay v-model="value2">
+            <CarouselItem>
+                <div class="demo-carousel">1</div>
+            </CarouselItem>
+            <CarouselItem>
+                <div class="demo-carousel">2</div>
+            </CarouselItem>
+            <CarouselItem>
+                <div class="demo-carousel">3</div>
+            </CarouselItem>
+            <CarouselItem>
+                <div class="demo-carousel">4</div>
+            </CarouselItem>
+        </Carousel>
+        </Col>
+        <Col span="6">
+            <Card>
+                <p slot="title">登录</p>
+                <Input v-model="loginname">
+                    <span slot="prepend">登录：</span>
+                </Input>
+                <br>
+                <Input v-model="passwd">
+                    <span slot="prepend">密码：</span>
+                </Input>
+            </Card>
+        </Col>
+    </Row>   
+</div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+            legendArr: [],
+            color: this.$store.state.color,
+            myChart: {},
+            geoCoordMap: {},
+            name: '散点图'}
+        },
+    }
+</script>
