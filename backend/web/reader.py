2e3a04bc130877a603a349ed08fc3a94712f8631
GitBaronYang <773280154@qq.com>
2018-03-27 20:06:59 +0800

    yangf 提交测试哦

--- backend/web/reader.py
+++ backend/web/reader.py
@@ -3,6 +3,14 @@
 """
 import requests
 
+headers = {  
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36",  
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",  
+    "Accept-Encoding": "gzip, deflate",  
+    "Host": "www.zhihu.com",  
+    "Upgrade-Insecure-Requests": "1",  
+} 
+
 def login(url):
     """
     处理登录部分
@@ -24,3 +32,6 @@ def write(str):
     写文件读取内容写入文件
     """
     pass
+
+if __name__ == '__main__':
+    print('run')
--- backend/web/test.py
+++ backend/web/test.py
@@ -1,5 +1,9 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-r = requests.get('https://github.com/timeline.json')
-print(r.content)
+headers={'Content-Type': 'application/json'}
+data = {'userName':'yangfei','passwd':'123456'}
+r = requests.post('http://192.168.1.38:8080/api/users/login',data=json.dumps(data),headers=headers)
+print(r.json())
+print(json.dumps(data))
