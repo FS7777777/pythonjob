8a0d1594872c03c04b04e066c22e497f36d80545
GitForShadow <773280154@qq.com>
2018-04-06 19:33:28 +0800

    yang add test

--- backend/web/test.py
+++ backend/web/test.py
@@ -1,8 +1,3 @@
-import requests
-import json
-from bs4 import BeautifulSoup
-from datafile import datalog
-r = requests.get('https://movie.douban.com/top250')
-print(r.content)
-datalog.write(r.content)
+from ctypes import *
 
+print(c_char_p(b"hello"))
