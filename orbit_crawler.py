35e6f4800b7e2306edb25eec8d20e9579df6e96d
yang <yangfei@castianta.com>
2020-09-06 12:44:51 +0800

    get all tle

--- orbit_crawler.py
+++ orbit_crawler.py
@@ -4,19 +4,13 @@ import random
 import functools
 import redis
 
-global REDIS_CLIENT
+global POOL
 
 
-def init_redis():
-    pool = redis.ConnectionPool(host='192.168.160.247', port=6379,
+def init_redis_pool():
+    global POOL
+    POOL = redis.ConnectionPool(host='192.168.160.247', port=6379,
                                 db=0, password='W03wx2020@redis')
-    global REDIS_CLIENT
-    REDIS_CLIENT = redis.Redis(connection_pool=pool)
-
-
-def close_redis():
-    global REDIS_CLIENT
-    REDIS_CLIENT.close()
 
 
 def fake_header():
@@ -53,13 +47,17 @@ async def http_client(host):
 
 
 def storage_tle(list_tle):
-    for temp in list_tle:
-        # 判断两行的有效性可以再完善下此处只做了长度校验
-        if len(temp) == 3:
-            # 获取两行第二行noradid
-            key = temp[2].split()[1]
-            global REDIS_CLIENT
-            REDIS_CLIENT.hset('orbit_global_tle', key, '\r\n'.join(temp))
+    global POOL
+    r = redis.Redis(connection_pool=POOL)
+    try:
+        for temp in list_tle:
+            # 判断两行的有效性可以再完善下此处只做了长度校验
+            if len(temp) == 3:
+                # 获取两行第二行noradid
+                key = temp[2].split()[1]
+                r.hset('orbit_global_tle', key, '\r\n'.join(temp))
+    finally:
+        r.close()
 
 
 def parse_tle(context):
@@ -73,12 +71,35 @@ def parse_tle(context):
 
 if __name__ == "__main__":
     # 初始化redis连接
-    init_redis()
+    init_redis_pool()
     # 获取数据
     loop = asyncio.get_event_loop()
     tasks = [http_client(host)
-             for host in ['https://www.celestrak.com/NORAD/elements/tle-new.txt']]
+             for host in ['https://www.celestrak.com/NORAD/elements/tle-new.txt', 'https://www.celestrak.com/NORAD/elements/stations.txt',
+                          'https://www.celestrak.com/NORAD/elements/visual.txt', 'https://www.celestrak.com/NORAD/elements/active.txt',
+                          'https://www.celestrak.com/NORAD/elements/analyst.txt', 'https://www.celestrak.com/NORAD/elements/2019-006.txt',
+                          'https://www.celestrak.com/NORAD/elements/1999-025.txt', 'https://www.celestrak.com/NORAD/elements/iridium-33-debris.txt',
+                          'https://www.celestrak.com/NORAD/elements/cosmos-2251-debris.txt', 'https://www.celestrak.com/NORAD/elements/weather.txt',
+                          'https://www.celestrak.com/NORAD/elements/noaa.txt', 'https://www.celestrak.com/NORAD/elements/goes.txt',
+                          'https://www.celestrak.com/NORAD/elements/resource.txt', 'https://www.celestrak.com/NORAD/elements/sarsat.txt',
+                          'https://www.celestrak.com/NORAD/elements/dmc.txt', 'https://www.celestrak.com/NORAD/elements/tdrss.txt',
+                          'https://www.celestrak.com/NORAD/elements/argos.txt', 'https://www.celestrak.com/NORAD/elements/planet.txt',
+                          'https://www.celestrak.com/NORAD/elements/spire.txt', 'https://www.celestrak.com/NORAD/elements/geo.txt',
+                          'https://www.celestrak.com/satcat/gpz.php', 'https://www.celestrak.com/satcat/gpz-plus.php',
+                          'https://www.celestrak.com/NORAD/elements/intelsat.txt', 'https://www.celestrak.com/NORAD/elements/ses.txt',
+                          'https://www.celestrak.com/NORAD/elements/iridium.txt', 'https://www.celestrak.com/NORAD/elements/iridium-NEXT.txt',
+                          'https://www.celestrak.com/NORAD/elements/starlink.txt', 'https://www.celestrak.com/NORAD/elements/oneweb.txt',
+                          'https://www.celestrak.com/NORAD/elements/orbcomm.txt', 'https://www.celestrak.com/NORAD/elements/globalstar.txt',
+                          'https://www.celestrak.com/NORAD/elements/amateur.txt', 'https://www.celestrak.com/NORAD/elements/x-comm.txt',
+                          'https://www.celestrak.com/NORAD/elements/other-comm.txt', 'https://www.celestrak.com/NORAD/elements/satnogs.txt',
+                          'https://www.celestrak.com/NORAD/elements/gorizont.txt', 'https://www.celestrak.com/NORAD/elements/raduga.txt',
+                          'https://www.celestrak.com/NORAD/elements/molniya.txt', 'https://www.celestrak.com/NORAD/elements/gps-ops.txt',
+                          'https://www.celestrak.com/NORAD/elements/glo-ops.txt', 'https://www.celestrak.com/NORAD/elements/galileo.txt',
+                          'https://www.celestrak.com/NORAD/elements/sbas.txt', 'https://www.celestrak.com/NORAD/elements/nnss.txt',
+                          'https://www.celestrak.com/NORAD/elements/musson.txt', 'https://www.celestrak.com/NORAD/elements/science.txt',
+                          'https://www.celestrak.com/NORAD/elements/geodetic.txt', 'https://www.celestrak.com/NORAD/elements/engineering.txt',
+                          'https://www.celestrak.com/NORAD/elements/education.txt', 'https://www.celestrak.com/NORAD/elements/military.txt',
+                          'https://www.celestrak.com/NORAD/elements/radar.txt', 'https://www.celestrak.com/NORAD/elements/cubesat.txt',
+                          'https://www.celestrak.com/NORAD/elements/other.txt']]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
-    # 关闭连接
-    close_redis()
--- redis_client.py
+++ redis_client.py
@@ -5,5 +5,6 @@ pool = redis.ConnectionPool(host='192.168.160.247', port=6379,
                             db=0, password='W03wx2020@redis')
 r = redis.Redis(connection_pool=pool)
 
+
 tle = r.hget('orbit_global_tle', '46232')
 print(tle)
