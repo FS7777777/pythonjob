51a7e89cb6f8d901d230605a1da1fb5c9b313bcc
yang <yangfei@castianta.com>
2020-09-06 12:01:19 +0800

    add redis

---
+++ redis_client.py
@@ -0,0 +1,9 @@
+import redis
+
+
+pool = redis.ConnectionPool(host='192.168.160.247', port=6379,
+                            db=0, password='W03wx2020@redis')
+r = redis.Redis(connection_pool=pool)
+
+tle = r.hget('orbit_global_tle', '46232')
+print(tle)
